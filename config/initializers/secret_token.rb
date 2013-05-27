6ec027afa22c840b684505853bb8f0b41c957582
Charlie Moseley <charlie@rofl.io>
2013-05-27 08:57:19 -0700

    New Rails 4.0.0.rc1 -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emotejiji::Application.config.secret_key_base = '7d574653f31011f8df94320eedf3c0e0dc6002257c7a608a9c5aa251e7ce859e6a35dbe6717c4bb99b53b3ee4ae6446bf4b887e6f5923e93ff8f7664e02bacd7'
