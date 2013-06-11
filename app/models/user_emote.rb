22f78eb299b44932cb900a2879ba721fd913107e
Charlie Moseley <charlie@rofl.io>
2013-06-11 08:42:44 -0700

    Basic validations for user_emote.

--- app/models/user_emote.rb
+++ app/models/user_emote.rb
@@ -1,2 +1,3 @@
 class UserEmote < ActiveRecord::Base
+  validates :kind, inclusion: { in: %w(owner favorite tagged) }
 end
--- test/models/user_emote_test.rb
+++ test/models/user_emote_test.rb
@@ -2,10 +2,32 @@ require "test_helper"
 
 describe UserEmote do
   before do
-    @user_emote = UserEmote.new
+    @emote = Emote.create text: "foobar", tags: { foo: 1, bar: 1 }
+    @user  = User.create  email: "foo@bar.com", username: "foobar",
+                          password: "randomfoo", password_confirmation: "randomfoo"
+
+    @user_emote = UserEmote.new kind: 'owner', user_id: @user.id, emote_id: @emote.id,
+                                tags: ['foo', 'bar']
   end
 
-  it "must be valid" do
+  it "should respond to it's attributes and be valid" do
+    attrs = [:kind, :user_id, :emote_id, :tags]
+    attrs.each do |attr|
+      @user_emote.must_respond_to attr
+    end
     @user_emote.valid?.must_equal true
+  end
+
+  it "should be invalid with a invalid kind" do
+    @user_emote.kind = nil;      @user_emote.valid?.must_equal false
+    @user_emote.kind = "foobar"; @user_emote.valid?.must_equal false
+  end
+
+  it "should be valid with a valid kind" do
+    valid = ['owner', 'favorite', 'tagged']
+    valid.each do |kind|
+      @user_emote.kind = kind
+      @user_emote.valid?.must_equal true
+    end
   end
 end
