58088ac61f22cec40c0cbce851bfe13247fbe851
Charlie Moseley <charlie@rofl.io>
2013-07-02 09:24:26 -0700

    Updated rails configuration to prevent sprockets from destroying angular code.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Emotejiji::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Prevent sprockets from butchering angular.
+  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(mangle: false) }
 end
